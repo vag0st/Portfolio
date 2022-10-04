08f5306068c426c5b77c9a3c2a2e60d0154ed3af
vag0st <zomgwtfwow@hotmail.com>
2022-10-05 00:38:26 +0100

    ADD Sidebar component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,58 @@
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+    faLinkedin,
+    faGithub,
+    faYoutube,
+    faSkype,
+  } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
-    return <>Hello from sidebar</>
+    return (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo" />
+            <img className='sub-logo' src={LogoSubtitle} alt="logo" />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active"  className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/ntsiavos-evangelos/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/vag0st'>
+                    <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.youtube.com/channel/UCc_dqlxl0gc0D8zHhLlXjNQ'>
+                    <FontAwesomeIcon icon={faYoutube} color="4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://join.skype.com/invite/JE1gF7UpuQK7'>
+                    <FontAwesomeIcon icon={faSkype} color="4d4d4e" />
+                </a>
+            </li>
+        </ul>
+    </div>
+    )
 }
 
 export default Sidebar
